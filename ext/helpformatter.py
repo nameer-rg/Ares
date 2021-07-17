9d3749930dfc0fc714c43e600bea9e8674ad864b
Daddie0 <killswitchbuisniss@gmail.com>
2021-07-18 01:42:23 +0200

    Change helpformatter footer

--- ext/helpformatter.py
+++ ext/helpformatter.py
@@ -4,4 +4,4 @@ from discord.ext.commands import DefaultHelpCommand
 
 class helpformatter(DefaultHelpCommand):
     def get_ending_note(self):
-        return "The shittest selfbot in existence by Daddie#6969\n\nWebsite > https://daddie.dev"
+        return "The shittest selfbot in existence by Daddie0\n\nWebsite > https://daddie.dev"
